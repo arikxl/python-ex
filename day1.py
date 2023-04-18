63f556768150be9f6bf8bd51137e70021ca280c6
Arik Alexandrov <arikxl@gmail.com>
2023-04-18 22:35:01 +0300

    finish day 1

--- day1.py
+++ day1.py
@@ -1,7 +1,43 @@
 
-# page 8 
-a=input('choose the first number: ');
-b=input('choose the second number: ');
-print(b,a);
-print(b,b,b);
+# ********page 8********
+# num1 = input('Choose the first number: ')
+# num2 = input('Choose the second number: ')
+# print(num2, num1)
+# print(num2, num2, num2)
 
+
+# **********page 12********
+# length = int(input('What is the length of the room? '))
+# width = int(input('What is the width of the room? '))
+# perimeter = str((length + width)*2)
+# area = str(length * width)
+# print('The perimeter of the room is: ' + perimeter)
+# print(f'The area of the room is: {area})
+
+
+# diameter = int(input('What is the diameter of the pot? '))
+# depth = int(input('What is the depth of the pot? '))
+# radius = diameter / 2
+# volume = str(3.14 * radius**2 * depth)
+# print(f'The volume of the pot is: {volume}')
+
+
+# length = int(input('Enter the length of the movie in minutes: '))
+# hours = length // 60
+# minutes = length % 60
+# print(f'The length of the movie is {hours} hours and {minutes} minutes.')
+
+
+# num = int(input('Choose a number with 4 digits: '))
+# second_digit_from_right = (num // 10) % 10
+# print(f'The second digit from the right is: {second_digit_from_right}')
+
+
+# num = int(input('Choose a three-digit number '))
+# hundred_digit = num // 100
+# print(f'The hundreds digit is: {hundred_digit}')
+
+
+# num = int(input('Choose a number between 10 and 99: '))
+# new_num = (num % 10) * 10 + (num // 10)
+# print(f'The new number is: {new_num}')
