b825d4b867e04ca523d41d33afc4ce6c632bc147
Arik Alexandrov <arikxl@gmail.com>
2023-05-10 23:45:18 +0300

    ex4

--- day3.py
+++ day3.py
@@ -82,10 +82,10 @@ def ex1():
 
 
 def ex2():
-    num1 = float(input('Enter number 1: '))
-    num2 = float(input('Enter number 2: '))
-    num3 = float(input('Enter number 3: '))
-    num4 = float(input('Enter number 4: '))
+    num1 = int(input('Enter number 1: '))
+    num2 = int(input('Enter number 2: '))
+    num3 = int(input('Enter number 3: '))
+    num4 = int(input('Enter number 4: '))
 
     average = (num1 + num2 + num3 + num4) / 4
 
@@ -103,7 +103,7 @@ def ex2():
 
 def ex3():
     num_of_checks = int(input('Enter number of checks: '))
-    total_amount = float(input('Enter total amount of checks without fees: $'))
+    total_amount = int(input('Enter total amount of checks without fees: $'))
 
     commission_of_num = 0.1 * num_of_checks
     if total_amount <= 500:
@@ -118,3 +118,30 @@ def ex3():
     print(f'Commission: ${total_commission}')
     print(f'Total payment: ${total_amount + total_commission}')
 # ex3()
+
+
+def ex4():
+    basic_salary = int(input('Basic salary: '))
+    seniority = int(input('Number of years in work: '))
+    children = int(input('Number of children: '))
+    hours_worked = int(input('Hours of work: '))
+
+    salary = basic_salary
+    if seniority > 10:
+        salary += basic_salary * 0.1
+
+    if children >=4 and children <7 :
+        salary += 400 * children
+    elif children >= 7:
+        salary += 700 * children
+
+    if hours_worked > 160:
+        extra_hours = hours_worked - 160
+        if extra_hours <= 16:
+            salary += extra_hours * 80
+        else:
+            salary += 16 * 80
+            salary += (extra_hours - 16) * 120
+
+    print(f'The total salary is: ${salary}')
+ex4()
