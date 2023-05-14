a56bd86f54b737ab96da550e00eb54333cab2361
Arik Alexandrov <arikxl@gmail.com>
2023-05-15 00:32:59 +0300

    pigs2

--- pig.py
+++ pig.py
@@ -8,4 +8,16 @@ for word in words:
     if word[0] in 'aeoiu':
         new_word = word + 'yay'
         new_words.append(new_word)
-        
+    else:
+        vowel_pos = 0
+        for letter in word:
+            if letter not in 'aeuio':
+                vowel_pos +=1
+            else:
+                break
+        cons = word[:vowel_pos]
+        the_rest = word[vowel_pos:]
+        new_word = the_rest + cons + 'yay'
+        new_words.append(new_word)
+        pig_words=" ".join(new_words)
+print(pig_words)
