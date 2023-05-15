192167f133381a7df81a1b6aae2280b0bae273ff
Arik Alexandrov <arikxl@gmail.com>
2023-05-16 00:31:43 +0300

    ex1 finally

--- day5.py
+++ day5.py
@@ -1,23 +1,14 @@
 # ********page 35********
 
 def ex1():
+    num = float('-inf')
     for i in range(1, 11):
-        num = 0
         new_num = int(input('Choose a number: '))
-        if new_num < num:
-            print(i)
+        if num > new_num:
+            # print(i)
             return 'Not Sorted'
+        else:
+            num = new_num
     return 'Sorted'
 print(ex1())
 
-
-def ex1(numbers):
-    for i in range(1, len(numbers)):
-        if numbers[i] < numbers[i-1]:
-            # print(i)
-            return 'Not Sorted'
-    return 'Sorted'
-
-print(ex1([1,2,9,4,5,6,7,8]))
-print('****')
-print(ex1([1,2,3,4,5,6,7,8]))
