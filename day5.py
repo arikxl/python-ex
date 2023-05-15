734d1ff74bc64bfbc7e08028a04cef0ee0695d10
Arik Alexandrov <arikxl@gmail.com>
2023-05-16 00:40:59 +0300

    p35 ex4

--- day5.py
+++ day5.py
@@ -10,5 +10,29 @@ def ex1():
         else:
             num = new_num
     return 'Sorted'
-print(ex1())
+# print(ex1())
 
+
+def ex4():
+    votes = {
+        1: 0,  # agree
+        2: 0,  # against
+        3: 0  # abstain
+    }
+
+    for i in range(1, 171):
+        vote = int(input('Enter vote code (1-4): '))
+        if vote == 1:
+            votes[1] += 1
+        elif vote == 2:
+            votes[2] += 1
+        elif vote == 3:
+            votes[3] += 1
+        elif vote == 4:
+            print(f'Vote of "Veto" was cast by country number: {i}.')
+            return
+
+    print('Number of "Agree" votes:', votes[1])
+    print('Number of "Against" votes:', votes[2])
+    print('Number of "Abstain" votes:', votes[3])
+ex4()
